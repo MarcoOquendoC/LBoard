7700c04dfb2678535a035bb2296f098097f81932
Marco Oquendo <marcooquendoc@gmail.com>
2022-08-25 15:21:01 -0500

    Get new game Id

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 // import _ from 'lodash';
 import './style.css';
 import ui from './modules/ui.js';
+import newGame from './modules/getNewGameId.js';
 
+const loadLocal = localStorage.getItem('List');
+if (loadLocal === null) {
+  newGame.getFirstId();
+}
 const scoreData = [
   { id: 0, name: 'Marco', score: 100 },
   { id: 0, name: 'Marco', score: 100 },
