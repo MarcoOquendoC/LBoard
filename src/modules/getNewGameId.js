2b009441a69c636c9568a7fdfff8bd71323f287b
Marco Oquendo <marcooquendoc@gmail.com>
2022-08-25 15:39:43 -0500

    Add module to get new Id

--- src/modules/getNewGameId.js
+++ src/modules/getNewGameId.js
@@ -1,17 +1,16 @@
-const newGame = {
-  url: 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/',
- 
-  async getFirstId() {
-    const response = await fetch(newGame.url, {
-      method: 'POST',
-      body: JSON.stringify({ "name": "SirenomanGame" }),
-      headers: { 'Content-type': 'application/json; charset=UTF-8' },
-    });
-    const data = await response.json();
-    const getId = data.result.split(' ')[3];
-    localStorage.setItem('List', getId);
-  },
-
+const newGame = async() => {
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+    method: 'POST',
+    body: JSON.stringify({ 
+      "name": "SirenomanGame" 
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  const data = await response.json();
+  const getId = data.result.split(' ')[3];
+  localStorage.setItem('List', getId);
 }
 
 export default newGame;
