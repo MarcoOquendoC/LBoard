47b18dc492af97b2470f9d722b9265c2aa4b6a6b
Marco Oquendo <marcooquendoc@gmail.com>
2022-08-26 00:32:31 -0500

    Add getScores method, minor changes in object of functions

--- src/modules/apiMethods.js
+++ src/modules/apiMethods.js
@@ -1,21 +1,23 @@
 const SirenomanGame = {
-  url: 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/',
-  id: 'Wixx5eHz4Mwu4bDNa1Gl',
-  idUrl: `${SirenomanGame.url}${SirenomanGame.id}/scores/`,
-  response: '',
-  async addScore(name, score) {
-    const response = await fetch(SirenomanGame.idUrl, {
+  async addScore(user, score) {
+    const idUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Wixx5eHz4Mwu4bDNa1Gl/scores/';
+    const response = await fetch(idUrl, {
       method: 'POST',
       body: JSON.stringify({
-        'name': name,
-        'score': score,
+        user,
+        score,
       }),
       headers: {
         'Content-type': 'application/json; charset=UTF-8',
       },
     });
-    SirenomanGame.response = response;
-  }
-}
+    return response.json();
+  },
+  async getScores() {
+    const idUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Wixx5eHz4Mwu4bDNa1Gl/scores/';
+    const response = await (await fetch(idUrl)).json();
+    return response.result;
+  },
+};
 
 export default SirenomanGame;
