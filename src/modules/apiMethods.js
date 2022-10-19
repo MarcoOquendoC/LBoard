1b73463decd0e429f030c29becb3e302f63b9205
Marco <marcooquendoc@gmail.com>
2022-10-19 13:42:05 -0500

    Update apiMethods.js

--- src/modules/apiMethods.js
+++ src/modules/apiMethods.js
@@ -1,6 +1,6 @@
 const SirenomanGame = {
   async addScore(user, score) {
-    const idUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Wixx5eHz4Mwu4bDNa1Gl/scores/';
+    const idUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/R96t6YalFyo06sqOglma/scores/';
     const response = await fetch(idUrl, {
       method: 'POST',
       body: JSON.stringify({
@@ -14,7 +14,7 @@ const SirenomanGame = {
     return response.json();
   },
   async getScores() {
-    const idUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Wixx5eHz4Mwu4bDNa1Gl/scores/';
+    const idUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/R96t6YalFyo06sqOglma/scores/';
     const response = await (await fetch(idUrl)).json();
     return response.result;
   },
