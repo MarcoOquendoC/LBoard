e85b4c80ae90155f2ba6af537a5ab4f13ce2efec
Marco <marcooquendoc@gmail.com>
2023-03-21 01:27:20 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import SirenomanGame from './modules/apiMethods.js';
 
 const manageData = async () => {
   const data = await SirenomanGame.getScores();
-  data.sort((a, b) => b.score - a.score);
   const topTen = data.slice(0, 100);
   ui.render(topTen);
 };
@@ -17,15 +16,15 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
   const user = document.getElementById('user').value;
   const score = document.getElementById('score').value;
-  if (user.length <= 15 && score < 1000) {
+  if (user.length <= 15 && score < 1000 && score > 1) {
     SirenomanGame.addScore(user, score).then(manageData);
     form.reset();
-  } else if (score < 1000) {
-    alert('Please enter a valid score and Stop messing with the application`s code');
+  } else if (score > 1000) {
+    alert('Please enter a valid score in the range 1..1000');
   } else if (user.length <= 15) {
-    alert('Please enter a valid name and Stop messing with the application`s code');
+    alert('Please enter a valid name');
   } else {
-    alert('Stop messing with the application`s code');
+    alert('Please enter a valid score in the range 1..1000 and a valid name');
   }
 });
 
